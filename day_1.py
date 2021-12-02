594cf3ad8cccfa65afd3bf001157b5bda8682097
jshapiro314 <jshapiro314@users.noreply.github.com>
2021-12-02 15:22:48 -0500

    black

--- day_1.py
+++ day_1.py
@@ -4,7 +4,7 @@ from typing import List
 def count_depth_increase(depths: List[int]) -> int:
     count = 0
     for i in range(1, len(depths)):
-        if depths[i] > depths[i-1]:
+        if depths[i] > depths[i - 1]:
             count += 1
 
     return count
@@ -14,14 +14,17 @@ def count_depth_sliding_window(depths: List[int]) -> int:
     # window size = 3
     count = 0
     for i in range(len(depths) - 3):
-        if (depths[i+1] + depths[i+2] + depths[i+3]) > (depths[i] + depths[i+1] + depths[i+2]):
+        if (depths[i + 1] + depths[i + 2] + depths[i + 3]) > (
+            depths[i] + depths[i + 1] + depths[i + 2]
+        ):
             count += 1
 
     return count
 
+
 if __name__ == "__main__":
     depths = []
-    with open('day_1_input.txt') as f:
+    with open("day_1_input.txt") as f:
         for line in f:
             depths.append(int(line.strip()))
 
